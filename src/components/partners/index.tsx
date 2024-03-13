e83343dc973dea52c5648ca1f340e65419d9ab52
Amal C P <acp070146@gmail.com>
2024-03-13 12:46:48 +0530

    home partners added

--- src/components/partners/index.tsx
+++ src/components/partners/index.tsx
@@ -1,8 +1,40 @@
+import styles from './index.module.css'
+import img1 from "./assets/img1.png";
+import img2 from "./assets/img2.png";
+import img3 from "./assets/img3.png";
+import img4 from "./assets/img4.png";
+import img5 from "./assets/img5.png";
+
+const data = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+];
 
 const Partners = () => {
   return (
-	<div>Partners</div>
-  )
-}
+    <div className={styles.Wrapper}>
+      {data.map((item, index) => (
+        <div key={index}>
+          {" "}
+          {/* Ensure each child in a list has a unique "key" prop. */}
+          <img src={item} alt={`Partner ${index + 1}`} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Partners
+export default Partners;
