496d1a3d998ccd87dc57ee9cbbaebf1c03812638
Amal C P <acp070146@gmail.com>
2024-03-13 11:03:39 +0530

    testimonial section updated

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import Partners from "../../components/partners";
 import Hero from "./components/hero";
+import { Testimonials } from "./components/testimonials";
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
 			<h1>Home</h1>
 			<Hero />
 			<Partners />
+			<Testimonials />
 		</div>
 	);
 };
