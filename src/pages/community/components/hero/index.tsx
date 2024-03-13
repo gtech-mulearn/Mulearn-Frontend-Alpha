18f5e6d4712575b2ccfe38e950dcebd3e96610aa
Amal C P <acp070146@gmail.com>
2024-03-13 18:31:06 +0530

    landing section of community updated

--- src/pages/community/components/hero/index.tsx
+++ src/pages/community/components/hero/index.tsx
@@ -1,9 +1,21 @@
-import styles from './index.module.css'
+import styles from "./index.module.css";
+import bg from "./bg.png";
 
-type Props = {}
+type Props = {};
 
 export const Landing = (_props: Props) => {
   return (
-    <div className={styles.Wrapper}>Landing</div>
-  )
-}
+    <div className={styles.Wrapper}>
+      <img src={bg} alt="" />
+      <div className={styles.Content}>
+        <h1>
+          <div>
+            <span>One</span>&nbsp;<div>Community,</div>
+          </div>{" "}
+          <p>countless stories</p>
+        </h1>
+        <p>Join the fast growing student community</p>
+      </div>
+    </div>
+  );
+};
--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -1,9 +1,11 @@
 import { PartnersLinear } from "../../components/partners";
+import { Landing } from "./components/hero";
 import WhyJoin from "./components/whyJoin";
 
 const Community = () => {
   return (
     <div>
+      <Landing />
       <PartnersLinear />
       <WhyJoin />
     </div>
